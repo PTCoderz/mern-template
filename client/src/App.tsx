4af88fc75f3583db630d4c0c03014cbe8a56b39c
Akseli Palmer <akseli@toddsden.com>
2022-09-21 18:11:53 -0500

    added tailwind styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,20 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
+    <div className="flex justify-center items-center flex-col h-screen bg-gradient-to-r from-blue-100 to-purple-200">
+      <div className="flex h-15">
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="w-40 m-10" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-40 m-10" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + TailWind</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+      <h1 className="text-2xl">Vite + React + TailWind</h1>
+      <div className="flex justify-center flex-col"></div>
+      <p className="text-lg mt-3">
         Click on the Vite and React logos to learn more
       </p>
     </div>
