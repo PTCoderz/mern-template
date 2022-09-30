a3a026bc1f1f0ebf0e6a0fafa62a2e0eb2aa6386
Akseli Palmer <akseli@toddsden.com>
2022-09-30 15:22:39 -0500

    created form input from default tailwind components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,13 @@
 import reactLogo from "./assets/react.svg";
 import "./index.css";
+import Example from "./assets/Example";
 
 import Navbar from "./assets/Navbar";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <div className="flex justify-center items-center flex-col h-screen bg-gradient-to-r from-blue-100 to-purple-200">
-        <div className="flex h-15">
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="w-40 m-10" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="w-40 m-10" alt="React logo" />
-          </a>
-        </div>
-        <h1 className="text-2xl">Vite + React + TailWind</h1>
-        <div className="flex justify-center flex-col"></div>
-        <p className="text-lg mt-3">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+    <div className="flex justify-center items-center flex-col h-screen bg-gradient-to-r from-blue-100 to-purple-200">
+      <Example />
     </div>
   );
 }
